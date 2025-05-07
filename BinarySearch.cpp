5ffa60281704200e6a1726dc08c8d4416bea6a71
Taradwimahendra15 <taradwimahendra15@gmail.com>
2025-05-07 12:21:31 +0700

    void binarysearch

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -53,6 +53,20 @@ void display()
     cout << "\n========================================\n";
     cout << "    Elemen Array Setelah Diurutkan (Asc)  \n";
     cout << "\n========================================\n";
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " --> ";
+        }
+    }
+    cout << endl;
+}
+
+void binarySearch()
+{
+    char ulang;
 }
  
 
