59772b67117c0fb7ced3e84e0ebb6d92b4040e8a
Taradwimahendra15 <taradwimahendra15@gmail.com>
2025-05-07 12:45:28 +0700

    int low and high untuk mencari elemen

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -67,6 +67,43 @@ void display()
 void binarySearch()
 {
     char ulang;
+    do
+    {
+        cout << "\n========================================\n";
+        cout << "          Pencarian Binary Search         \n";
+        cout << "\n========================================\n";
+        cout << "Masukkan Elemen yang ingin di cari : ";
+        cin >> x;
+
+        int low = 0;
+        int high = nPanjang - 1;
+
+        do
+        {
+            int mid = (low + high) / 2;
+            if (element[mid] == x)
+            {
+                cout << "\n[✔] Elemen " << " ditemukan pada indeks " << mid << ".\n";
+                return;
+            }
+            if (x < element[mid])
+            {
+                high = mid - 1;
+            }
+            if (x < element[mid])
+            {
+                low = mid + 1;
+            }
+        } while (low <= high);
+
+        if (low > high)
+        {
+            cout << "\n[✖] Elemen " << x << " tidak ditemukan dalam array\n";
+        }
+        cout << "\nIngin mencari lagi? (y/n) : ";
+        cin >> ulang;               
+    } while (ulang == 'y' || ulang == 'Y');
+    
 }
  
 
